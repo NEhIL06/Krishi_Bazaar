31e32305014c7735c86aa628368d04ffdf22a908
Nehil <chnadrakarnehil.06112002@gmail.com>
2025-07-29 17:23:34 +0530

    appwrite config update

--- src/config/appwrite.ts
+++ src/config/appwrite.ts
@@ -4,9 +4,9 @@ import { Client, Account, Databases, Storage, Functions, ID } from 'react-native
 const client = new Client();
 
 client
-  .setEndpoint(process.env.APPWRITE_ENDPOINT || 'https://fra.cloud.appwrite.io/v1')
-  .setProject(process.env.APPWRITE_PROJECT_ID || '687defff0035523d7f27')
-  .setPlatform(process.env.APPWRITE_PLATFORM || 'com.example.krishibazar');
+  .setEndpoint(process.env.APPWRITE_ENDPOINT as string)
+  .setProject(process.env.APPWRITE_PROJECT_ID as string)
+  .setPlatform(process.env.APPWRITE_PLATFORM as string);
 
 // Initialize services
 export const account = new Account(client);
@@ -15,17 +15,17 @@ export const storage = new Storage(client);
 export const functions = new Functions(client);
 
 // Database and collection IDs
-export const DATABASE_ID = process.env.APPWRITE_DATABASE_ID || "68849ccf000725f375e5" as string;
-export const STORAGE_BUCKET_ID = process.env.APPWRITE_STORAGE_BUCKET_ID || "68849cef001df89be612" as string ;
+export const DATABASE_ID = process.env.APPWRITE_DATABASE_ID as string;
+export const STORAGE_BUCKET_ID = process.env.APPWRITE_STORAGE_BUCKET_ID as string ;
 
 export const COLLECTION_IDS = {
-  CATEGORIES: process.env.CATEGORIES_COLLECTION_ID || '68873a840017ad9cb4a4',
-  FARMERS: process.env.FARMERS_COLLECTION_ID || '68873a62001f447ef53f',
-  MESSAGES: process.env.MESSAGES_COLLECTION_ID || '68873a970039225e85a7',
-  ORDERS: process.env.ORDERS_COLLECTION_ID || '68873aa8003724631e5c',
-  PRODUCTS: process.env.PRODUCTS_COLLECTION_ID || '68873ab20023a17f9431',
-  REVIEWS: process.env.REVIEWS_COLLECTION_ID || '68873ab900299b95f096',
-  USERS: process.env.USERS_COLLECTION_ID || '6884a0b50020a181ca67',
+  CATEGORIES: process.env.CATEGORIES_COLLECTION_ID as string,
+  FARMERS: process.env.FARMERS_COLLECTION_ID as string,
+  MESSAGES: process.env.MESSAGES_COLLECTION_ID as string,
+  ORDERS: process.env.ORDERS_COLLECTION_ID as string,
+  PRODUCTS: process.env.PRODUCTS_COLLECTION_ID as string,
+  REVIEWS: process.env.REVIEWS_COLLECTION_ID as string,
+  USERS: process.env.USERS_COLLECTION_ID as string,
 };
 
 export { ID };
